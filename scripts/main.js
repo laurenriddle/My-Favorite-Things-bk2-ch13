fdc8e1b0f7cbeb5f37ce0d95475e7485305f9ed3
Lauren Riddle <lari7132@gmail.com>
2019-10-17 11:57:58 -0500

    edited js so that if the input field is blank, it will not put anything in the DOM and it will alert the user to enter something

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,15 @@ document.querySelector("#saveEntry").addEventListener("click", event => {
         Collect the user put by selecting the input fields, one
         at a time, and accessing the `value` property
     */
-    const thingName = document.querySelector("#name").value
-    const vendor = document.querySelector("#location").value
 
+    if (document.querySelector("#name").value === '' || document.querySelector("#location").value === '') {
+    alert("Please fill out all fields.")
+    } else {
     // Once you have collected all the values, update the DOM
     // with some HTML
+    const thingName = document.querySelector("#name").value
+    const vendor = document.querySelector("#location").value
+    
     container.innerHTML += `
         <section>
             <h1>${thingName}</h1>
@@ -21,4 +25,5 @@ document.querySelector("#saveEntry").addEventListener("click", event => {
     document.querySelector("#location").value = ''
     document.querySelector("#name").focus()
 
+    }
 })
